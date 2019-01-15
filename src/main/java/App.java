475c13776a1067a3229370a511654445afbcfd52
JoshDowling <jk2md@live.co.uk>
2019-01-15 14:20:47 +0000

    Added functions = add account, retrieve accounts.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,5 +4,14 @@ public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
+		
+		Account josh = new Account("Josh", "Dowling", 1);
+		Account joel = new Account("Joel", "Halford", 2);
+		
+		Service newService = new Service();
+		newService.addAccount(josh);
+		newService.addAccount(joel);
+		
+		newService.getAccounts();
 	}
 }
